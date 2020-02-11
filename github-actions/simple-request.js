304def7207b12b553c4031610ca6a0d67b72b54d
Cynthia Kiser <cynthiakiser@gmail.com>
2020-02-10 22:49:02 -0800

    pick the data we need out of the languate and contributors calls

--- github-actions/simple-request.js
+++ github-actions/simple-request.js
@@ -14,14 +14,18 @@ var github = {
     }).then(function(body) {
       let apiData = {};
       let language_urls = [];
+      let contributor_urls = [];
       body.items.forEach(function(project) {
         apiData[project.id] = {name: project.name,
                                languages: {url: project.languages_url},
                                contributors: {url: project.contributors_url}
                               };
         language_urls.push(project.languages_url);
+        contributor_urls.push(project.contributors_url);
       });
-      return language_urls;
+      return contributor_urls;
+    }).catch(function(err) {
+      return err.message;
     });
   },
 
@@ -34,14 +38,40 @@ var github = {
         "User-Agent": "Hack For LA"
       }
     }).then(function(body) {
-      return body;
+      return Object.keys(body);
+    }).catch(function(err) {
+      return err.message;
+    });
+  },
+
+  getContributorsInfo: function(url) {
+    return request({
+      "method":"GET",
+      "uri": url,
+      "json": true,
+      "headers": {
+        "User-Agent": "Hack For LA"
+      }
+    }).then(function(body) {
+      let contributors = [];
+      body.forEach(function(user) {
+        contributors.push({"id": user.id,
+                           "github_url": user.html_url,
+                           "avatar_url": user.avatar_url,
+                           "gravatar_id": user.gravatar_id
+                          });
+      });
+      return contributors;
+    }).catch(function(err) {
+      return err.message;
     });
   }
+
 }
 
 function main(params) {
   return github.getAllTaggedRepos()
-    .map(github.getLanguageInfo);
+    .map(github.getContributorsInfo);
 }
 
 main().then(function(result) {
