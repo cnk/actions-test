4d2acc0fdebcab62f69f04e8718abaa40e403345
Cynthia Kiser <cynthiakiser@gmail.com>
2020-02-11 21:52:26 -0800

    Steven Ettinger version 1 - all data shows as {"isFulfilled":false,"isRejected":false}

--- github-actions/simple-request.js
+++ github-actions/simple-request.js
@@ -3,84 +3,85 @@
 var request = require('request-promise');
 
 var github = {
-  token: null,
-
-  getAllTaggedRepos: function() {
-    return request({
-      "method":"GET",
-      "uri": "https://api.github.com/search/repositories?q=topic:hack-for-la&sort=updated&order=desc",
-      "json": true,
-      "headers": {
-        "Authorization": "token " + github.token,
-        "User-Agent": "Hack For LA"
-      }
-    }).then(function(body) {
-      let apiData = {};
-      let language_urls = [];
-      let contributor_urls = [];
-      body.items.forEach(function(project) {
-        apiData[project.id] = {name: project.name,
-                               languages: {url: project.languages_url,
-                                           data: []},
-                               contributors: {url: project.contributors_url}
-                              };
-        language_urls.push(project.languages_url);
-        contributor_urls.push(project.contributors_url);
-      });
-      return language_urls;
-    }).catch(function(err) {
-      return err.message;
-    });
-  },
-
-  getLanguageInfo: function(url) {
-    return request({
-      "method":"GET",
-      "uri": url,
-      "json": true,
-      "headers": {
-        "Authorization": "token " + github.token,
-        "User-Agent": "Hack For LA"
-      }
-    }).then(function(body) {
-      return Object.keys(body);
-    }).catch(function(err) {
-      return err.message;
-    });
-  },
-
-  getContributorsInfo: function(url) {
-    return request({
-      "method":"GET",
-      "uri": url,
-      "json": true,
-      "headers": {
-        "Authorization": "token " + github.token,
-        "User-Agent": "Hack For LA"
-      }
-    }).then(function(body) {
-      let contributors = [];
-      body.forEach(function(user) {
-        contributors.push({"id": user.id,
-                           "github_url": user.html_url,
-                           "avatar_url": user.avatar_url,
-                           "gravatar_id": user.gravatar_id
-                          });
-      });
-      return contributors;
-    }).catch(function(err) {
-      return err.message;
-    });
-  }
-
+    token: null,
+    apiData: {},
+    getAllTaggedRepos: function() {
+        return request({
+            "method": "GET",
+            "uri": "https://api.github.com/search/repositories?q=topic:hack-for-la&sort=updated&order=desc",
+            "json": true,
+            "headers": {
+                "Authorization": "token " + github.token,
+                "User-Agent": "Hack For LA"
+            }
+        }).then(function(body) {
+            //let apiData = {};
+            let language_urls = [];
+            let contributor_urls = [];
+            body.items.forEach(function(project) {
+                github.apiData[project.id] = {
+                    name: project.name,
+                    languages: {
+                        url: project.languages_url,
+                        data: []
+                    },
+                    contributors: { url: project.contributors_url, data: [] }
+                };
+            });
+            return github.apiData;
+        }).catch(function(err) {
+            return err.message;
+        });
+    },
+    getLanguageInfo: function(url) {
+        return request({
+            "method": "GET",
+            "uri": url,
+            "json": true,
+            "headers": {
+                "Authorization": "token " + github.token,
+                "User-Agent": "Hack For LA"
+            }
+        }).then(function(body) {
+            return Object.keys(body);
+        }).catch(function(err) {
+            return err.message;
+        });
+    },
+    getContributorsInfo: function(url) {
+        return request({
+            "method": "GET",
+            "uri": url,
+            "json": true,
+            "headers": {
+                "Authorization": "token " + github.token,
+                "User-Agent": "Hack For LA"
+            }
+        }).then(function(body) {
+            let contributors = [];
+            body.forEach(function(user) {
+                contributors.push({
+                    "id": user.id,
+                    "github_url": user.html_url,
+                    "avatar_url": user.avatar_url,
+                    "gravatar_id": user.gravatar_id
+                });
+            });
+            return contributors;
+        }).catch(function(err) {
+            return err.message;
+        });
+    }
 }
-
-function main(params) {
-  github.token = params.token
-  return github.getAllTaggedRepos()
-    .map(github.getLanguageInfo);
+async function main(params) {
+    github.token = params.token
+    await github.getAllTaggedRepos()
+    let projectIds = Object.keys(github.apiData)
+    for (i = 0; i < projectIds.length; i++) {
+        github.apiData[projectIds[i]].languages.data = github.getLanguageInfo(github.apiData[projectIds[i]].languages.url)
+        github.apiData[projectIds[i]].contributors.data = github.getContributorsInfo(github.apiData[projectIds[i]].contributors.url)
+    }
+    let output = JSON.stringify(github.apiData);
+    console.log(output)
 }
-
-main({"token": process.argv[2]}).then(function(result) {
-  console.log(result);
-});
+main({ "token": process.argv[2] })
